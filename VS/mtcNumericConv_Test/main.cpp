84e153df6f900a7879dc75b60b6d446d89a324a4
LGgram <LGgram@LG>
2019-06-17 00:02:15 +0900

    数値-文字列のテストを実装

--- VS/mtcNumericConv_Test/main.cpp
+++ VS/mtcNumericConv_Test/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 #include<string>
 
 #include"mtcNumericConv.h"
@@ -28,8 +28,95 @@ bool test2() {
 	return false;
 }
 
+bool test3() {
+	double v = 1.23;
+	basic_string<char> str = ToString<char>(v);
+	return false;
+}
+
+bool test4() {
+	int src = 0x12345678;
+	basic_string<char> str = ToString<char>(src, 10);
+	if (str == "305419896") return true;
+
+	return false;
+
+}
+
+bool test5() {
+	int src = 0x12345678;
+	basic_string<char> str = ToString<char>(src, 16);
+	if (str == "12345678") return true;
+
+	return false;
+
+}
+
+bool test6() {
+	int src = 0x12345678;
+	basic_string<char> str = ToString<char>(src, 2);
+	if (str == "00010010001101000101011001111000") return true;
+
+	return false;
+
+}
+
+bool test7() {
+	unsigned int src = 0xFFFFFFFF;
+	basic_string<char> str = ToString<char>(src, 10);
+	if (str == "4294967295") return true;
+
+	return false;
+
+}
+
+bool test8() {
+	int src = 0x12345678;
+	basic_string<char8_t> str = ToString<char8_t>(src, 10);
+	if (str == u8"305419896") return true;
+
+	return false;
+
+}
+
+bool test9() {
+	int src = 0x12345678;
+	basic_string<char8_t> str = ToString<char8_t>(src, 16);
+	if (str == u8"12345678") return true;
+
+	return false;
+
+}
+
+bool test10() {
+	int src = 0x12345678;
+	basic_string<char8_t> str = ToString<char8_t>(src, 2);
+	if (str == u8"00010010001101000101011001111000") return true;
+
+	return false;
+
+}
+
+bool test11() {
+	unsigned int src = 0xFFFFFFFF;
+	basic_string<char8_t> str = ToString<char8_t>(src, 10);
+	if (str == u8"4294967295") return true;
+
+	return false;
+
+}
+
 int main() {
 	test1();
 	test2();
+	test3();
+	test4();
+	test5();
+	test6();
+	test7();
+	test8();
+	test9();
+	test10();
+	test11();
 	return 0;
 }
